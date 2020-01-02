83dd04692c4fa72a5e635026210136b52200a44b
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2020-01-02 19:05:51 +0530

    :art: change notifier provider for theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:dynamic_theme/screens/home.dart';
+import 'package:dynamic_theme/services/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider<ThemeChanger>.value(
+      value: ThemeChanger(themeData: ThemeData.light()),
+      child: MaterialAppWithTheme(),
+    );
+  }
+}
+
+class MaterialAppWithTheme extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeChanger>(context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Dynamic Theming',
+      theme: theme.getTheme(),
       home: HomePage(),
     );
   }
