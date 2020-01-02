8eb61eaefaaa5c7a85d15f3e1173e5291536c6a1
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2020-01-02 19:18:50 +0530

    :art: toggle theme on checkbox

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,19 @@
+import 'package:dynamic_theme/services/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
+    ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Dynamic Theming"),
@@ -20,6 +31,33 @@ class HomePage extends StatelessWidget {
                 letterSpacing: 2.0
               ),
             ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Dark Mode",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                Checkbox(
+                  value: isChecked,
+                  onChanged: (bool value) {
+                    setState(() {
+                      isChecked = value;
+                    });
+
+                    // Toggle Theme based on the checkbox current value
+                    if(isChecked) {
+                      _themeChanger.setTheme(ThemeData.dark());
+                    } else {
+                      _themeChanger.setTheme(ThemeData.light());
+                    }
+                  },
+                )
+              ],
+            )
           ],
         ),
       ),
